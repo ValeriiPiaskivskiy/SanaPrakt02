332479b097b2786bf81428b01dbf170aff6ec120
Valerii Piaskivskiy <sana22_pvo1@student.ztu.edu.ua>
2022-11-09 17:51:17 +0200

    Last 2 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Довільний текст в першому рядку");
 Console.WriteLine("Довільний текст в другому рядку");
 Console.WriteLine("Довільний текст в третьому рядку");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільний текст в пятому рядку");
+Console.WriteLine("Довільний текст в третьому рядку");
