d8cb1b7e74731f511006dc850417cbe90b839259
Valerii Piaskivskiy <sana22_pvo1@student.ztu.edu.ua>
2022-11-09 17:49:44 +0200

    First 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільний текст в першому рядку");
+Console.WriteLine("Довільний текст в другому рядку");
+Console.WriteLine("Довільний текст в третьому рядку");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
